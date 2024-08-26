c91137cf6bb2689dc0c9e394d1e0c7f4b30a58fe
fromddy <ddy_mainland@163.com>
2024-08-26 10:12:43 +0800

    feat: introduce nextra-theme-docs

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Dark Forest Ares Documentation</span>,
+    project: {
+      link: 'https://github.com/dfarchon/DFAres-docs'
+    }
+    // ... other theme options
+  }
